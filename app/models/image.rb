1b3bea4f0a4e2f230a8d185c191292e957c8de2f
Don <don@bahamallama.com>
2015-05-23 08:18:29 -0400

    increased the image size for extrapic to 600px

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,6 @@
 class Image < ActiveRecord::Base
   if Rails.env.development?
-    has_attached_file :extrapic, :styles => { :large => "400x", :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+    has_attached_file :extrapic, :styles => { :large => "600x", :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
     validates_attachment_content_type :extrapic, :content_type => /\Aimage\/.*\Z/
   else
     has_attached_file :extrapic, :styles => { :large => "400x", :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg", 
