c733ae0b7debb1e045d29c8dbc8da3357664563a
Don <don@bahamallama.com>
2015-06-03 19:30:20 -0400

    log in fix

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-#Need to add domain: :all to try to solve mobile log in issues
-Rails.application.config.session_store :cookie_store, key: '_autodealer_session', domain: :all
+Rails.application.config.session_store :cookie_store, key: '_autodealer_session'
