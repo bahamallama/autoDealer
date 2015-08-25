3624b19ef7456b47306cf4e174d1d75e6c9625ce
Don <don@bahamallama.com>
2015-08-25 07:34:36 -0400

    added relationship to Models with Makes, updated views for models

--- app/models/make.rb
+++ app/models/make.rb
@@ -1,5 +1,6 @@
 class Make < ActiveRecord::Base
   has_many :cars
+  has_many :models
   
   default_scope {order("Lower(name)")}
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,5 +1,6 @@
 class Model < ActiveRecord::Base
   has_many :cars
+  belongs_to :make
   
   default_scope {order("Lower(name)")}
 end
