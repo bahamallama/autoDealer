bacf9668e877e6567e2ce2f224158fe850c2f79b
Don <don@bahamallama.com>
2015-09-04 21:44:44 -0400

    trying to fix deploy issues

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,12 @@
-# config valid only for current version of Capistrano
-lock '3.4.0'
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-# set :branch, "master"
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/home/#{user}/apps/#{application}'
-
-# Default value for :scm is :git
-set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+set :repo_url,        'ssh://git@github.com/bahamallama/autoDealer.git'
+set :application,     'jsAuto'
+set :user,            'deployer'
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
 
 # Change these
-#server 'bahamallama.cc', user: 'deployer', port: 30000, roles: [:web, :app, :db], primary: true
 server 'bahamallama.cc', port: 30000, roles: [:web, :app, :db], primary: true
+
 set :repo_url,        'ssh://git@github.com/bahamallama/autoDealer.git'
 set :application,     'jsAuto'
 set :user,            'deployer'
@@ -44,7 +17,6 @@ set :puma_workers,    0
 set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
-set :branch,          'master'
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
