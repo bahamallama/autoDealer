c24982b088228027e9918903138b5760330f19f0
Don <don@bahamallama.com>
2016-01-12 15:44:33 -0500

    fixed a jumbtron issue where on small windows or mobile browsers the featured car overlaped the first listed car

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150913050135) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cars", force: :cascade do |t|
     t.integer  "make_id"
     t.integer  "model_id"
@@ -84,7 +87,7 @@ ActiveRecord::Schema.define(version: 20150913050135) do
     t.integer  "role_id"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
