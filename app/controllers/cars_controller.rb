f7209ed7a3878a7abc5236169408295c7c0bc456
Don <don@bahamallama.com>
2016-01-15 10:53:54 -0500

    added pagination

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -26,7 +26,7 @@ class CarsController < ApplicationController
   # GET /cars
   # GET /cars.json
   def index
-    @cars = Car.where(:published => true)
+    @cars = Car.where(:published => true).paginate(:page => params[:page], :per_page => 2)
   end
 
   # GET /cars/1
