7b1c31a128a5e5850aded35dc980139f65522037
Don <don@bahamallama.com>
2016-02-17 14:27:17 -0500

    modified deploy.rb to add figaro tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -60,6 +60,18 @@ namespace :deploy do
     end
   end
 
+  namespace :figaro do
+    desc "SCP transfer figaro configuration to the shared folder"
+    task :setup do
+      transfer :up, "config/application.yml", "#{shared_path}/application.yml", :via => :scp
+    end
+
+    desc "Symlink application.yml to the release path"
+    task :finalize do
+      run "ln -sf #{shared_path}/application.yml #{release_path}/config/application.yml"
+    end
+  end
+
   desc 'Initial Deploy'
   task :initial do
     on roles(:app) do
