3007070af13b29f3c9254092c9608f0b1f3e9248
Don <don@bahamallama.com>
2016-08-22 22:19:51 -0400

    cap compile is crap trying to fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -59,6 +59,27 @@ namespace :deploy do
       end
     end
   end
+  
+  namespace :assets do
+     task :precompile, :roles => :web, :except => { :no_release => true } do
+       # If this is our first deploy - don't check for the previous version
+       if remote_file_exists?(current_path)
+         from = source.next_revision(current_revision)
+         if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+           run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+         else
+           logger.info "Skipping asset pre-compilation because there were no asset changes"
+         end
+       else
+         run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+       end
+     end
+   end
+
+ # Helper function
+ def remote_file_exists?(full_path)
+   'true' ==  capture("if [ -e #{full_path} ]; then echo 'true'; fi").strip
+ end
 
   namespace :figaro do
     desc "SCP transfer figaro configuration to the shared folder"
