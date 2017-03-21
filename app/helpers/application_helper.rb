1f18704ec54ad553282c733f9ac68afb11aa4d3d
Don <don@bahamallama.com>
2017-03-21 15:30:04 -0400

    added a helper function to calculate width for footer images (warranty and finance) and added responsive image design to show car and sold, featured, sale and redesigned the site index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def human_boolean(boolean)
       boolean ? 'Yes' : 'No'
   end
+  
+  def finance_and_warranty
+    if (@site.financeImage? && @site.warrantyImage?)
+      html = "<div class='col-md-6 center-block' style='float:none; display:inline-block; vertical-align:middle; margin-right:-4px;'>"
+      html.html_safe
+    else
+      html = "<div class='col-md-12 center-block' style='float:none; display:inline-block; vertical-align:middle; margin-right:-4px;'>"
+      html.html_safe
+    end
+  end
 end
