1d4efa1d9ea05d69ebbca7eff49df49d7ab832c1
BahamaLlama <don@bahamallama.com>
2017-11-02 15:23:10 -0400

    deploy assets fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -60,33 +60,6 @@ namespace :deploy do
       end
     end
   end
-  
-  
-    namespace :assets do
-
-      Rake::Task['deploy:assets:precompile'].clear_actions
-
-      desc 'Precompile assets locally and upload to servers'
-      task :precompile do
-        on roles(fetch(:assets_roles)) do
-          run_locally do
-            with rails_env: fetch(:rails_env) do
-              execute 'bin/rake assets:precompile'
-            end
-          end
-
-          within release_path do
-            with rails_env: fetch(:rails_env) do
-              upload!('./public/assets/', "#{shared_path}/public/", recursive: true)
-            end
-          end
-
-          run_locally { execute 'rm -rf public/assets' }
-        end
-      end
-
-    end
-
 
   namespace :figaro do
     desc "SCP transfer figaro configuration to the shared folder"
@@ -119,7 +92,6 @@ namespace :deploy do
   after  :finishing,    :cleanup
   after  :finishing,    :restart
 end
-
 
 # ps aux | grep puma    # Get puma pid
 # kill -s SIGUSR2 pid   # Restart puma
